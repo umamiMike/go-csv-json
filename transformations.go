4bebf416162f2cf1330634070d20d45c27b08448
Mike Chastain <mchastain@fuelmedical.com>
2018-02-26 18:33:22 -0800

    adds scaffold for transformation functions

--- transformations.go
+++ transformations.go
@@ -4,6 +4,32 @@ import "strings"
 
 type String string
 
+type TransformFunc func(kind string)
+type TransformType int
+
+const (
+	A initType = iota
+	B
+	C
+	D
+	MaxTransformTypes
+)
+
+var transformFuncs = map[transformTyp]TransformFunc{
+	A: trimMrnFromColon,
+}
+
+func initTransformations() {
+	for t := A; t < MaxTransformTypes; t++ {
+		f, ok := transformFuncs[t]
+		if ok {
+			f(t)
+		} else {
+			fmt.Println("No function defined for type", t)
+		}
+	}
+}
+
 /**
 situation of returning the right side of a string formatted like "#####:B####"
 will return "B#####"
