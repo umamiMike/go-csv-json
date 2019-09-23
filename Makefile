c27303b3844d42cb51ed6e7a2c53c6f1e41436f8
Michael Wilding <michaelwchastain@gmail.com>
2019-09-22 22:24:53 -0700

    updating libs

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
-BUILDFILE="posty"
+BUILDFILE="go-csv-json"
+PHONY="build"
 build:
 	go build -o ${BUILDFILE} 
-	@echo "posty build complete"
+	@echo "${BUILDFILE} build complete"
 build-shrink:
 	go build -o ${BUILDFILE} 
 	upx --brute ${BUILDFILE}
-	@echo "posty build complete"
+	@echo "${BUILDFILE}  build complete"
 build-linux:
 	GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ./linux/${BUILDFILE}
 	upx --brute ./linux/${BUILDFILE}
--- utils/utils.go
+++ utils/utils.go
@@ -1,14 +1,14 @@
-package main
+package utils
 
 import "net/http"
 import "net/url"
 import "strings"
 
-func buildURLData(d *url.Values, k string, v string) {
+func BuildURLData(d *url.Values, k string, v string) {
 	d.Set(k, v)
 }
 
-func makeRequest(data url.Values) *http.Request {
+func MakeRequest(data url.Values) *http.Request {
 	req, _ := http.NewRequest("POST", runningConfig.Host+runningConfig.Endpoint, strings.NewReader(data.Encode()))
 	for _, header := range runningConfig.Headers {
 		if header.Type == "Cookie" {
